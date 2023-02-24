5f4c40e1ae171aed192759bc0238f770a6ba5cd9
Clarisse Scofield <clarisse.scofield@gmail.com>
2023-02-24 15:19:54 -0300

    Create binarySearch.py

---
+++ binarySearch/binarySearch.py
@@ -0,0 +1,24 @@
+"""
+Leetcode Problem
+https://leetcode.com/problems/binary-search/
+"""
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] < target:
+                l = mid + 1
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                return mid
+
+        return -1
