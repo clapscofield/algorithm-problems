b97ceb9e5df8f6500de5f2b1315c50fc4362d866
Clarisse Scofield <clarisse.scofield@gmail.com>
2023-02-25 08:04:42 -0300

    Create searchInRotateArray.py

---
+++ binarySearch/searchInRotateArray.py
@@ -0,0 +1,33 @@
+"""
+Leetcode Problem
+https://leetcode.com/problems/search-in-rotated-sorted-array/
+"""
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if target == nums[mid]:
+                return mid
+            
+            #left sorted portion
+            if nums[mid] >= nums[l]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1 
+                else:
+                    r = mid - 1
+            #right sorted portion
+            else:
+                if target < nums[mid] or target < nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid - 1
+        
+        return -1
