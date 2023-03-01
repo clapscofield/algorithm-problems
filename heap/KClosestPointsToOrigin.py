ab62140b6bdf2f0f8949905ecdcb5dac2ad0c7a0
Clarisse Scofield <clarisse.scofield@gmail.com>
2023-03-01 14:30:53 -0300

    Create KClosestPointsToOrigin.py

---
+++ heap/KClosestPointsToOrigin.py
@@ -0,0 +1,26 @@
+"""
+Leetcode Problem
+https://leetcode.com/problems/k-closest-points-to-origin/
+"""
+
+class Solution(object):
+    def kClosest(self, points, k):
+        """
+        :type points: List[List[int]]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        minHeap = []
+        for x,y in points:
+            dist = x ** 2 + y ** 2
+            minHeap.append([dist, x, y])
+
+        heapq.heapify(minHeap)
+        res = []
+        while k > 0:
+            dist, x, y = heapq.heappop(minHeap)
+            res.append([x,y])
+            k -=1
+
+        return res
+
