876c335bd4e64a506c99898b48cbd98ae1caa593
Clarisse Scofield <clarisse.scofield@gmail.com>
2023-03-07 21:06:08 -0300

    Create jumpGame.py

---
+++ greedy/jumpGame.py
@@ -0,0 +1,18 @@
+"""
+Leetcode Problem
+https://leetcode.com/problems/jump-game/
+"""
+
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        goal = len(nums) - 1
+
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+            
+        return True if goal == 0 else False
